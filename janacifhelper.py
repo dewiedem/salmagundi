aaa758558b0dbd41b40e4e20299d004535b36f77
Dennis Wiedemann <dennis.wiedemann@chem.tu-berlin.de>
2019-04-29 02:57:03 +0200

    Getting it done

--- janacifhelper.py
+++ janacifhelper.py
@@ -30,6 +30,8 @@ __status__ = 'Development'
 
 OUTPUT_FILENAME = 'JANA_CIF_Helper.cif'  # Name of the output CIF file
 BLOCK_NAME = 'global'                    # Name of the CIF data block to store information in
+PHASE_NUMBER = 1                         # Number of phase to put information out on
+
 
 class Suppressor(object):
     """Suppress text output to `stdout`.
@@ -60,9 +62,7 @@ def rnd_sigfig(value, digits):
     return round(value, -int(math.floor(math.log10(abs(value))) - (digits - 1)))
 
 
-
-
-def rnd_iucr(average, su):
+def rnd_iucr(avg, su):
     """Round a value according to IUCr rules.
 
     :param avg: average or central value
@@ -76,6 +76,18 @@ def rnd_iucr(average, su):
     pass
 
 
+def ordinal(n):
+    """Return an ordinal number representation for a given cardinal.
+
+    :param n: cardinal number
+    :type n: int
+    :return: ordinal number
+    :rtype: str
+    """
+
+    return '%d%s' % (n, 'tsnrhtdd'[(math.floor(n // 10) % 10 != 1) * (n % 10 < 4) * n % 10::4])
+
+
 # Parse arguments
 parser = argparse.ArgumentParser(description='Covert parameters from JANA2006 output into standardized CIF items.')
 parser.add_argument('input', metavar='input file', type=argparse.FileType('rb'),
@@ -84,24 +96,53 @@ parser.add_argument('-v', '--version', action='version', version=__version__)
 args = parser.parse_args()
 
 # Construct file names
-file_stem = os.path.splitext(args.input.name)[0]
-file_m41 = file_stem + '.m41'
-file_ref = file_stem + '.ref'
-file_cif = os.path.join(os.path.split(file_stem)[0], OUTPUT_FILENAME)
+name_stem = os.path.splitext(args.input.name)[0]
+name_m41 = name_stem + '.m41'
+name_ref = name_stem + '.ref'
+name_cif = os.path.join(os.path.split(name_stem)[0], OUTPUT_FILENAME)
 
 # Greeting
 print("Hiiieee! My name is yours, what's JANA CIF Helper?\n")
 
 # Scan for and read the values from *.m41
 print('Reading from *.m41 ...', end='')
-with open(file_m41, 'r') as read_file:
-    pass
+skipped = []
+phase_select = []
+with open(name_m41, 'r') as read_file:
+    for line in read_file:
+        if line.startswith('bckgtype'):
+            select = dict(zip(line.split()[::2], line.split()[1::2]))
+        elif line.startswith('absor'):
+            select.update(zip(line.split()[::2], line.split()[1::2]))
+        elif line.startswith('skipfrto'):
+            skipped.append((line.split()[1], line.split()[2]))
+        elif line.startswith('phase'):
+            line = read_file.readline() + read_file.readline()
+            phase_select.append(dict(zip(line.split()[::2], line.split()[1::2])))
+        elif line.startswith('end'):
+            break
+    while not line.startswith('# Shift parameters'):
+        line = read_file.readline()
+    shift = dict(zip(['zero', 'sycos', 'sysin'], read_file.readline().split()[0:3]))  # TODO: not split, 9 numbers
+    while not line.startswith('# Asymmetry'):
+        line = read_file.readline()
+    if select['asymm'] == '1':
+        asymm = read_file.readline()[0:9]
+print(select, phase_select, shift, asymm)
 print(' Done.')
 
 # Scan for and read the values from *.ref
 print('Reading from *.ref ...', end='')
-with open(file_ref, 'r') as read_file:
-    pass
+phase_count = 0
+with open(name_ref, 'r') as read_file:
+    for line in read_file:
+        if line.startswith('Cycle  RFobs'):
+            phase_count = +1
+            if phase_count == PHASE_NUMBER:
+                while line != '\n':
+                    line_old = line
+                    line = read_file.readline()
+                rb_obs = float(line_old.split()[9])
 print(' Done.')
 
 # Construct CIF
@@ -122,65 +163,43 @@ cif_block['_audit_creation_method'] = 'JANA CIF Helper v.' + __version__
 cif_block['_audit_creation_date'] = datetime.datetime.now().strftime('%Y-%m-%d')
 
 # Store extracted items
-#_exptl_absorpt_coefficient_mu       0.009
-#_pd_char_atten_coef_mu_calc         0.0082
-#_exptl_absorpt_correction_type      cylinder
-#_exptl_absorpt_process_details
-#;
-#correction for a cylindrical sample with \mR = 0.041 as implemented in
-# JANA2006 (Pet\<r\'i\<cek et al., 2014)
-#;
-#_refine_ls_R_I_factor               ?
-#loop_
-#_pd_proc_info_excluded_regions
-# 'from 0.05 to 7.50: irregular onset'
-# 'from 158.0 to 160.11: cut-off refelection'
-#_pd_proc_ls_special_details
-#;
-#zero - point
-#correction:
-#;
-#_pd_proc_ls_profile_function
-#;
-#pseudo - Voigt
-#profile in TCH
-#approach(Thompson
-#et
-#al., 1987):
-#G
-#~U
-#~ =, G
-#~V
-#~ =, G
-#~W
-#~ =, L
-#~X
-#~ =, L
-#~Y
-#~ = 0;
-#asymmetry
-#correction
-#according
-#to
-#Howard(1982):
-#P =
-#;
+if select['absor'] == '1':
+    cif_block['_exptl_absorpt_correction_type'] = 'cylinder'
+    cif_block['_exptl_absorpt_process_details'] = '\ncorrection for a cylindrical sample with \\mR = 0.041 as ' \
+                                                  'implemented in JANA2006 (Pet\\<r\\\'i\\<cek et al., 2014)'
 
-#_pd_proc_ls_background_function
-#;
-#manual
-#background(visual
-#estimation, unrefined) interpolated
-#by
-#ten
-#Legendre
-#polynomials(1
-#st:, 2
-#nd:, 3
-#rd:, 4
-#th:, 5
-#th: )
-#;
+cif_block['_refine_ls_R_I_factor'] = '{:2.4f}'.format(rb_obs / 100)
+
+skipped_string = []
+for region in skipped:
+    skipped_string.append('from {:3.2f} to {:3.2f}\\%: '.format(float(region[0]), float(region[1])))
+    cif_block['_pd_proc_info_excluded_regions'] = skipped_string
+    cif_block.CreateLoop(['_pd_proc_info_excluded_regions'])
+
+pd_proc_ls_special_details = '\n'
+if shift['zero'] != '0.000000':
+    pd_proc_ls_special_details += 'zero-point correction: ' + shift['zero']
+cif_block['_pd_proc_ls_special_details'] = pd_proc_ls_special_details
+
+_pd_proc_ls_profile_function = '\n'
+if phase_select[PHASE_NUMBER-1]['proffun'] == '3':
+    _pd_proc_ls_profile_function += 'pseudo-Voigt profile according to Thompson, Cox & Hastings (1987): '
+# pseudo-Voigt profile according to Thompson, Cox & Hastings (1987): G~U~ =, G~V~ =, G~W~ =, L~X~ =, L~Y~ = 0;
+if select['asymm'] == '1':
+    _pd_proc_ls_profile_function += ';\nasymmetry correction according to Howard (1982): P = ' + asymm  # TODO: value with esd
+cif_block['_pd_proc_ls_profile_function'] = _pd_proc_ls_profile_function
+
+_pd_proc_ls_background_function = '\n'
+if select['manbckg'] == '1':
+    _pd_proc_ls_background_function += 'manual background (visual estimation, unrefined)'
+if select['manbckg'] == '1' and select['bckgtype'] == '1':
+    _pd_proc_ls_background_function += ' interpolated by '
+if select['bckgtype'] == '1':
+    _pd_proc_ls_background_function += select['bckgnum'] + ' Legendre polynomials (1st: '
+for i in range(2, int(select['bckgnum']) + 1):
+    _pd_proc_ls_background_function += ', {:s}: '.format(ordinal(i)) + '0.00'  # TODO: actual values
+_pd_proc_ls_background_function += ')'
+cif_block['_pd_proc_ls_background_function'] = _pd_proc_ls_background_function
 print(' Done.')
 
 # Store static items
@@ -191,7 +210,7 @@ cif_block['_refine_ls_weighting_details'] = 'w=1/[\\s^2^(F~o~)+(0.01F~o~)^2^]'  
 
 # Output CIF
 print('Writing to %s ...' % OUTPUT_FILENAME, end='')
-with open(file_cif, 'w') as write_file, Suppressor():
+with open(name_cif, 'w') as write_file, Suppressor():
     write_file.write(cif.WriteOut())
 print(' Done.')
 
