486c78cc72fbe622d2027f93824ec8320b7e2ef4
Dennis Wiedemann <dennis.wiedemann@chem.tu-berlin.de>
2019-06-11 20:52:09 +0200

    Finished routines

--- janacifhelper.py
+++ janacifhelper.py
@@ -31,6 +31,7 @@ __status__ = 'Development'
 OUTPUT_FILENAME = 'JANA_CIF_Helper.cif'  # Name of the output CIF file
 BLOCK_NAME = 'global'                    # Name of the CIF data block to store information in
 PHASE_NUMBER = 1                         # Number of phase to put information out on
+NUM_LEN = 9                              # Length of numbers (in characters) stored in *.m41
 
 
 class Suppressor(object):
@@ -69,30 +70,54 @@ def iucr_string(values):
     :return: IUCr compliant representation
     :rtype: str
     """
+    if values[1] == 0 or values[1] is None:  # No or zero s.u. given
+        return str(values[0])
+
     sig_pos = math.floor(math.log10(abs(values[1])))  # position of first significant digit
-    sig_3 = math.trunc(values[1] * 10 ** (2 - sig_pos)) / 10 ** (2 - sig_pos)  # first three significant digits of s.u.
+    sig_3 = math.trunc(abs(values[1]) * 10 ** (2 - sig_pos)) / 10 ** (2 - sig_pos)  # 1st three significant s.u. digits
     sig_3 *= 10 ** -(sig_pos + 1)  # s.u. moved directly behind decimal separator (final range: 0.100-0.999)
 
     if sig_3 < 0.195:  # round to two digits (final s.u. range: 0.10-0.19)
-        su = round(values[1], 1 - sig_pos)
+        su = round(abs(values[1]), 1 - sig_pos)
         avg = round(values[0], 1 - sig_pos)
         sig_len = 2
     elif sig_3 < 0.950:  # round to one digit (final s.u. range: 0.2-0.9)
-        su = round(values[1], -sig_pos)
+        su = round(abs(values[1]), -sig_pos)
         avg = round(values[0], -sig_pos)
         sig_len = 1
     else:  # round to two digits and move forward (final s.u.: 0.10)
-        su = 0.10
-        avg = round(values[0], -1 - sig_pos)
+        sig_pos += 1
+        su = round(abs(values[1]), 1 - sig_pos)
+        avg = round(values[0], 1 - sig_pos)
         sig_len = 2
 
     if sig_pos > 0:  # only integral part for s.u. >= 1.95
-        avg_str = ('{:' + str(sig_pos) + '.0f}').format(avg).strip()
-        su_str = ('{:' + str(sig_pos) + '.0f}').format(abs(su))
+        sign_shift = -1 if values[0] < 0 else 0
+        avg_str = ('{:' + str(sig_pos + sign_shift) + '.0f}').format(avg).strip()
+        su_str = ('{:' + str(sig_pos) + '.0f}').format(su)
     else:  # fractional and possibly integral part for s.u. < 1.95
         avg_str = ('{:.' + str(-sig_pos + sig_len - 1) + 'f}').format(avg)
         su_str = '{:.0f}'.format(abs(su / 10 ** (sig_pos - sig_len + 1)))
+
     return '{:s}({:s})'.format(avg_str, su_str)
+
+
+def nibble_numbers(input_line, count, length=NUM_LEN):
+    """Return fixed-length numbers cut from the beginning of a string.
+
+    :param input_line: line to nibble the numbers from
+    :type input_line: str
+    :param count: numbers of numbers to nibble
+    :type count: int
+    :param length: length of the nibbles
+    :type length: int
+    :return: numbers
+    :rtype: list(floats)
+    """
+    numbers = []
+    for i in range(0, count):
+        numbers.append(float(input_line[i * length:(i + 1) * length]))
+    return numbers
 
 
 # Parse arguments
@@ -116,6 +141,8 @@ print('Reading from *.m41 ...', end='')
 skipped = []
 phase_select = []
 with open(name_m41, 'r') as read_file:
+
+    # Read header values (selections)
     for line in read_file:
         if line.startswith('bckgtype'):
             select = dict(zip(line.split()[::2], line.split()[1::2]))
@@ -128,15 +155,83 @@ with open(name_m41, 'r') as read_file:
             phase_select.append(dict(zip(line.split()[::2], line.split()[1::2])))
         elif line.startswith('end'):
             break
-    while not line.startswith('# Shift parameters'):
+
+    # Read shift parameters
+    while not line.startswith('# Shift'):
         line = read_file.readline()
-    # TODO: do not split, read nine fixed-length numbers
-    shift = dict(zip(['zero', 'sycos', 'sysin'], read_file.readline().split()[0:3]))
+    shift = dict(zip(['zero', 'sycos', 'sysin'], nibble_numbers(read_file.readline(), 3)))
+
+    # Read background parameters
+    while not line.startswith('# Background'):
+        line = read_file.readline()
+    background = []
+    for i in range(0, math.trunc(int(select['bckgnum']) / 6)):
+        background.append(nibble_numbers(read_file.readline(), 6))
+    if (int(select['bckgnum']) % 6) != 0:
+        background.append(nibble_numbers(read_file.readline(), int(select['bckgnum']) % 6))
+    background = [item for sublist in background for item in sublist]
+
+    # Read asymmetry parameters
     while not line.startswith('# Asymmetry'):
         line = read_file.readline()
     if select['asymm'] == '1':
-        asymm = read_file.readline()[0:9]
-print(select, phase_select, shift, asymm)
+        asymm = nibble_numbers(read_file.readline(), 1)[0]
+
+    # Read profile parameters
+    phase_count = 0
+    profile = {}
+    while not phase_count == PHASE_NUMBER:
+        line = read_file.readline()
+        while not line.startswith('### phase'):
+            line = read_file.readline()
+        phase_count += 1
+    while not line.startswith('# Gaussian'):
+        line = read_file.readline()
+    for key, value in zip(['GU', 'GV', 'GW', 'GP'], nibble_numbers(read_file.readline(), 4)):
+        profile[key] = value
+    while not line.startswith('# Lorentzian'):
+        line = read_file.readline()
+    for key, value in zip(['LX', 'LXe', 'LY', 'LYe'], nibble_numbers(read_file.readline(), 4)):
+        profile[key] = value
+
+    # Read shift s.u.'s
+    while not line.startswith('# Shift'):
+        line = read_file.readline()
+    shift_su = dict(zip(['zero', 'sycos', 'sysin'], nibble_numbers(read_file.readline(), 3)))
+
+    # Read background s.u.'s
+    while not line.startswith('# Background'):
+        line = read_file.readline()
+    background_su = []
+    for i in range(0, math.trunc(int(select['bckgnum']) / 6)):
+        background_su.append(nibble_numbers(read_file.readline(), 6))
+    if (int(select['bckgnum']) % 6) != 0:
+        background_su.append(nibble_numbers(read_file.readline(), int(select['bckgnum']) % 6))
+    background_su = [item for sublist in background_su for item in sublist]
+
+    # Read asymmetry s.u.'s
+    while not line.startswith('# Asymmetry'):
+        line = read_file.readline()
+    if select['asymm'] == '1':
+        asymm_su = nibble_numbers(read_file.readline(), 1)[0]
+
+    # Read profile s.u.'s
+    phase_count = 0
+    profile_su = {}
+    while not phase_count == PHASE_NUMBER:
+        line = read_file.readline()
+        while not line.startswith('### phase'):
+            line = read_file.readline()
+        phase_count += 1
+    while not line.startswith('# Gaussian'):
+        line = read_file.readline()
+    for key, value in zip(['GU', 'GV', 'GW', 'GP'], nibble_numbers(read_file.readline(), 4)):
+        profile_su[key] = value
+    while not line.startswith('# Lorentzian'):
+        line = read_file.readline()
+    for key, value in zip(['LX', 'LXe', 'LY', 'LYe'], nibble_numbers(read_file.readline(), 4)):
+        profile_su[key] = value
+
 print(' Done.')
 
 # Scan for and read the values from *.ref
@@ -145,7 +240,7 @@ phase_count = 0
 with open(name_ref, 'r') as read_file:
     for line in read_file:
         if line.startswith('Cycle  RFobs'):
-            phase_count = +1
+            phase_count += 1
             if phase_count == PHASE_NUMBER:
                 while line != '\n':
                     line_old = line
@@ -186,17 +281,31 @@ for region in skipped:
 
 pd_proc_ls_special_details = '\n'
 if shift['zero'] != '0.000000':
-    # TODO: write value with s.u. here
-    pd_proc_ls_special_details += 'zero-point correction: ' + shift['zero']
+    pd_proc_ls_special_details = 'zero-point correction: ' + iucr_string((shift['zero'], shift_su['zero']))
 cif_block['_pd_proc_ls_special_details'] = pd_proc_ls_special_details
 
 _pd_proc_ls_profile_function = '\n'
-if phase_select[PHASE_NUMBER-1]['proffun'] == '3':
-    _pd_proc_ls_profile_function += 'pseudo-Voigt profile according to Thompson, Cox & Hastings (1987): '
-# pseudo-Voigt profile according to Thompson, Cox & Hastings (1987): G~U~ =, G~V~ =, G~W~ =, L~X~ =, L~Y~ = 0;
+if phase_select[PHASE_NUMBER - 1]['proffun'] == '3':
+    _pd_proc_ls_profile_function += 'pseudo-Voigt profile according to Thompson, Cox & Hastings (1987): G~U~ = '
+    _pd_proc_ls_profile_function += '0' if profile['GU'] == 0.0 else iucr_string((profile['GU'], profile_su['GU']))
+    _pd_proc_ls_profile_function += ', G~V~ = '
+    _pd_proc_ls_profile_function += '0' if profile['GV'] == 0.0 else iucr_string((profile['GV'], profile_su['GV']))
+    _pd_proc_ls_profile_function += ', G~W~ = '
+    _pd_proc_ls_profile_function += '0' if profile['GW'] == 0.0 else iucr_string((profile['GW'], profile_su['GW']))
+    if profile['GP'] != 0.0:
+        _pd_proc_ls_profile_function += ', G~P~ = ' + iucr_string((profile['GP'], profile_su['GP']))
+    _pd_proc_ls_profile_function += ', L~X~ = '
+    _pd_proc_ls_profile_function += '0' if profile['LX'] == 0.0 else iucr_string((profile['LX'], profile_su['LX']))
+    _pd_proc_ls_profile_function += ', L~Y~ = '
+    _pd_proc_ls_profile_function += '0' if profile['LY'] == 0.0 else iucr_string((profile['LY'], profile_su['LY']))
+    if profile['LXe'] != 0.0:
+        _pd_proc_ls_profile_function += ', L~Xe~ = ' + iucr_string((profile['LXe'], profile_su['LXe']))
+    if profile['LYe'] != 0.0:
+        _pd_proc_ls_profile_function += ', L~Ye~ = ' + iucr_string((profile['LYe'], profile_su['LYe']))
+
 if select['asymm'] == '1':
-    # TODO: write value with s.u. here
-    _pd_proc_ls_profile_function += ';\nasymmetry correction according to Howard (1982): P = ' + asymm
+    _pd_proc_ls_profile_function += ';\nasymmetry correction according to Howard (1982): P = ' \
+                                   + iucr_string((asymm, asymm_su))
 cif_block['_pd_proc_ls_profile_function'] = _pd_proc_ls_profile_function
 
 _pd_proc_ls_background_function = '\n'
@@ -205,11 +314,12 @@ if select['manbckg'] == '1':
 if select['manbckg'] == '1' and select['bckgtype'] == '1':
     _pd_proc_ls_background_function += ' interpolated by '
 if select['bckgtype'] == '1':
-    _pd_proc_ls_background_function += select['bckgnum'] + ' Legendre polynomials (1st: '
+    _pd_proc_ls_background_function += select['bckgnum'] + ' Legendre polynomials [1st: ' \
+                                       + iucr_string((background[0], background_su[0]))
 for i in range(2, int(select['bckgnum']) + 1):
-    # TODO: write actual values
-    _pd_proc_ls_background_function += ', {:s}: '.format(ordinal(i)) + '0.00'
-_pd_proc_ls_background_function += ')'
+    _pd_proc_ls_background_function += ', {:s}: '.format(ordinal(i)) \
+                                       + iucr_string((background[i - 1], background_su[i - 1]))
+_pd_proc_ls_background_function += ']'
 cif_block['_pd_proc_ls_background_function'] = _pd_proc_ls_background_function
 print(' Done.')
 
